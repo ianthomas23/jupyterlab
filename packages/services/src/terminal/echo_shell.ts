d9782828440d7ff4fb9c824a06f4c5b5d6f9adc5
Ian Thomas <ianthomas23@gmail.com>
2024-04-11 12:40:51 +0100

    Colored prompt, command to upper case

--- packages/services/src/terminal/echo_shell.ts
+++ packages/services/src/terminal/echo_shell.ts
@@ -9,7 +9,7 @@ export class EchoShell {
   constructor(signal: Signal<ITerminalConnection, IMessage>) {
     console.log("==> EchoShell constructor");
     this._current_line = "";
-    this._prompt = "js-shell:$ "
+    this._prompt = "\x1b[1;31mjs-shell:$\x1b[1;0m "
     this._signal = signal;
 
     /*if (this._prompt) {
@@ -19,7 +19,7 @@ export class EchoShell {
   }
 
   // For jupyter-server-terminals it is terminado that handles messages.
-  receive(message: IMessage): void {
+  async receive(message: IMessage) {
     console.log("message_type", message.type);
     if (message.type == "set_size") {
 
@@ -27,14 +27,18 @@ export class EchoShell {
     else if (message.type == "stdin") {
       if (message.content) {
         const char = message.content[0] as string;
-        if (char.charCodeAt(0) == 13) {
+
+        // Also need to handle arrow keys, delete, etc.
+        // See https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+        //#if (char.charCodeAt(0) == 13) {
+        if (char[0] == "\r") {
           this.send_stdout(["\r\n"]);
 
-          // await results of running command.
-          const res = this._run_command(this._current_line);
+          const res = await this._run_command(this._current_line);
           if (res) {
             this.send_stdout([res + "\r\n"]);
           }
+          this._current_line = "";
 
           this.send_stdout([this._prompt]);
         } else {
@@ -47,10 +51,8 @@ export class EchoShell {
     }
   }
 
-  _run_command(command: string): string {
-
-    return "COMMAND RESULT";
-
+  async _run_command(command: string): Promise<string> {
+    return command.toUpperCase();
   }
 
   send_stdout(content: JSONPrimitive[]): void {
